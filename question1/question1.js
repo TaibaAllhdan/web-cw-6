bf27569e34b10078be6a96417f351f64b5bd331b
TaibaAllhdan <76259135+TaibaAllhdan@users.noreply.github.com>
2021-01-03 21:42:13 +0300

    done

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,14 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let name = prompt('what your name?')
+
+console.log(name)
+
+function sayhello() {
+    alert(`Hello ${name}`);
+}
+
+if(name){
+    sayhello()
+} else{
+    alert('invalid input')
+}
